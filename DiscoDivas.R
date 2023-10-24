2653fa5cf903d6164c4c0771d0ab67ec8352259b
Yunfeng Ruan <ryf.alexandria@gmail.com>
2023-10-24 19:34:42 -0400

    Update DiscoDivas.R

--- DiscoDivas.R
+++ DiscoDivas.R
@@ -3,7 +3,36 @@ library(dplyr)
 library(stringr)
 library(rio)
 library(optparse)
-source("./functions.r")
+
+Euclidean <- function(vec1, vec2, n) {
+sqr.sum = 0
+for (i in seq(n)) {sqr.sum <- sqr.sum + (vec1[i]-vec2[i])^2}
+dis <- sqrt(sqr.sum)
+return(dis)}
+
+Euclidean.matrix <- function(mat, vec, n) {
+N=nrow(mat)
+sqr.sum = matrix(rep(0, N), nrow = N, byrow=TRUE)
+for (i in seq(n)) {
+PC <- as.double(vec[i])
+sqr.sum <- sqr.sum + (mat[,i]-PC)^2}
+dis <- sqrt(sqr.sum)
+return(dis)}
+
+shrinkage.vector <- function(med, n) {
+N <- nrow(med)
+dis.list <- vector()
+for (i in seq(N*N)){
+row = (i-1) %/% N +1
+col = (i-1) %% N +1 
+dis.list[i] <- Euclidean(med[row,], med[col,], n)
+}
+dis.list <- unlist(dis.list)
+A <- matrix(dis.list, nrow=N, byrow=TRUE)
+A.inv <- solve(A)
+a <- A.inv %*% rep(1,N)
+return(a)}
+
 
 option_list = list(
   make_option(c("-m", "--med.file"), type="character", default=NULL,
