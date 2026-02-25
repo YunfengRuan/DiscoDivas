ab619305aea8b226c7a7e0a0e5fb75258c004757
Yunfeng Ruan <ryf.alexandria@gmail.com>
2026-02-25 07:16:48 -0500

    Update run.PRS-CS.sh

--- supplemental/run.PRS-CS.sh
+++ supplemental/run.PRS-CS.sh
@@ -1,1 +1,8 @@
-
+python $PRScs \
+ --ref_dir=${ref_dir} \
+ --bim_prefix=${bim_prefix} \
+ --sst_file=${work_dir}/formated.gwas/${dat}.chr${chr} \
+ --n_gwas=${n_trn} \
+ --chrom=${chr} \
+ --phi=${phi} \
+ --out_dir=${work_dir2}/posterior/PRS-CS.${dat}
